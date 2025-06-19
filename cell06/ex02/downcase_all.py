8d2f5442a7d0944386f2815b7050bc2356fb70d2
pornwasa <nutcharoenpornwasa@gmail.com>
2025-06-19 12:12:07 +0000

    o

--- cell06/ex02/downcase_all.py
+++ cell06/ex02/downcase_all.py
@@ -1,14 +1,3 @@
-#!/usr/bin/env python3
-import sys
 
-def downcase_it(s):
-    return s.lower()
-
-if __name__ == "__main__":
-    if len(sys.argv) == 1:
-        print("none")
-    else:
-        for arg in sys.argv[1:]:
-            print(downcase_it(arg))
 
 
