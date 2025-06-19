ce66cf0edfa2c6648ee710afac68c29385f162c2
pornwasa <nutcharoenpornwasa@gmail.com>
2025-06-19 16:17:30 +0000

    7.1

---
+++ cell07/ex01/family_affairs.py
@@ -0,0 +1,16 @@
+def find_the_redheads(family_dict):
+    # ใช้ filter เพื่อเลือก key ที่มี value เป็น "red"
+    return list(filter(lambda name: family_dict[name] == "red", family_dict))
+
+
+# ตัวอย่างสำหรับทดสอบ
+if __name__ == "__main__":
+    dupont_family = {
+        "florian": "red",
+        "marie": "blond",
+        "virginie": "brunette",
+        "david": "red",
+        "franck": "red"
+    }
+
+    print(find_the_redheads(dupont_family))
