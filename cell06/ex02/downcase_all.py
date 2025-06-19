eaf37232caef4645d9194d926adc22dcf7a17eb2
pornwasa <nutcharoenpornwasa@gmail.com>
2025-06-19 11:44:22 +0000

    m

--- cell06/ex02/downcase_all.py
+++ cell06/ex02/downcase_all.py
@@ -1,11 +1,14 @@
-import sys  # ใช้สำหรับรับ argument จาก command line
+#!/usr/bin/env python3
+import sys
 
-def downcase_it(text):
-    return text.lower()
+def downcase_it(s):
+    return s.lower()
 
-if len(sys.argv) == 1:
-    print("none")
-else:
-    for arg in sys.argv[1:]:
-        print(downcase_it(arg))
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("none")
+    else:
+        for arg in sys.argv[1:]:
+            print(downcase_it(arg))
+
 
