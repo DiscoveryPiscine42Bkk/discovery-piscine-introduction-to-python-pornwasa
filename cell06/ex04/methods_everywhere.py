9bc4efcfec989b90b1c4a6d2401520c23eea3012
pornwasa <nutcharoenpornwasa@gmail.com>
2025-06-19 12:29:25 +0000

    p

---
+++ cell06/ex04/methods_everywhere.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import sys
+
+def shrink(s):
+    print(s[:8])
+
+def enlarge(s):
+    print(s + 'Z' * (8 - len(s)))
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("none")
+    else:
+        for arg in sys.argv[1:]:
+            if len(arg) > 8:
+                shrink(arg)
+            elif len(arg) < 8:
+                enlarge(arg)
+            else:
+                print(arg)
